a575cebd2264d737236923114949093b11142946
Astro <astro@spaceboyz.net>
2016-01-20 16:46:16 +0100

    buffer: getters/setters for buffer flags

--- src/buffer.rs
+++ src/buffer.rs
@@ -15,6 +15,29 @@ impl Drop for Buffer{
     }
 }
 
+macro_rules! gst_buffer_flag {
+    ($getter:ident, $setter:ident, $flag:path) => (
+        pub fn $getter(&self) -> bool {
+            let flags = unsafe { (*self.gst_buffer()).mini_object.flags };
+            flags & $flag != 0
+        }
+
+        pub fn $setter(&mut self, value: bool) {
+            if !value {
+                /* Clear */
+                unsafe {
+                    (*self.gst_buffer_mut()).mini_object.flags &= !$flag;
+                }
+            } else {
+                /* Set */
+                unsafe {
+                    (*self.gst_buffer_mut()).mini_object.flags |= $flag;
+                }
+            }
+        }
+    )
+}
+
 impl Buffer{
     pub unsafe fn new(buffer: *mut GstBuffer, owned: bool) -> Option<Buffer>{
 		if buffer != ptr::null_mut(){
@@ -82,6 +105,22 @@ impl Buffer{
     pub fn gst_buffer_mut(&mut self) -> *mut GstBuffer{
         self.buffer
     }
+
+    pub fn flags(&self) -> guint {
+        unsafe { (*self.gst_buffer()).mini_object.flags }
+    }
+
+    gst_buffer_flag!(is_live, set_live, GST_BUFFER_FLAG_LIVE);
+    gst_buffer_flag!(is_decode_only, set_decode_only, GST_BUFFER_FLAG_DECODE_ONLY);
+    gst_buffer_flag!(is_discont, set_discont, GST_BUFFER_FLAG_DISCONT);
+    gst_buffer_flag!(is_resync, set_resync, GST_BUFFER_FLAG_RESYNC);
+    gst_buffer_flag!(is_corrupted, set_corrupted, GST_BUFFER_FLAG_CORRUPTED);
+    gst_buffer_flag!(is_marker, set_marker, GST_BUFFER_FLAG_MARKER);
+    gst_buffer_flag!(is_header, set_header, GST_BUFFER_FLAG_HEADER);
+    gst_buffer_flag!(is_gap, set_gap, GST_BUFFER_FLAG_GAP);
+    gst_buffer_flag!(is_droppable, set_droppable, GST_BUFFER_FLAG_DROPPABLE);
+    gst_buffer_flag!(is_delta_unit, set_delta_unit, GST_BUFFER_FLAG_DECODE_ONLY);
+    gst_buffer_flag!(is_tag_memory, set_tag_memory, GST_BUFFER_FLAG_TAG_MEMORY);
 }
 
 impl ::Transfer<GstBuffer> for Buffer{
