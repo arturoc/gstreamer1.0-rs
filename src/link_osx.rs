25a10e1a9274bf64947eca784e7e528f191650cf
David Rajchenbach-Teller <david.teller@univ-orleans.fr>
2016-06-21 13:37:15 +0200

    [DO NOT LAND] #14 - This builds on my machine

--- src/link_osx.rs
+++ src/link_osx.rs
@@ -1,2 +1,10 @@
-#[link(name = "GStreamer", kind = "framework")]
+//#[link(name = "GStreamer", kind = "framework")]
+//extern {}
+
+#[link(name = "gstvideo-1.0")]
+#[link(name = "gstapp-1.0")]
+#[link(name = "gstbase-1.0")]
+#[link(name = "gstreamer-1.0")]
+#[link(name = "gobject-2.0")]
+#[link(name = "glib-2.0")]
 extern {}
